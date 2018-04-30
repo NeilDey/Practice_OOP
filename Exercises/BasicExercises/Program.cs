df55d56596ddd5d1086a97292f76be49ecbe5795
Neil Dey <neilrdey@gmail.com>
2018-04-30 16:54:20 +0530

    Basic Exercises : Program 4

--- Exercises/BasicExercises/Program.cs
+++ Exercises/BasicExercises/Program.cs
@@ -15,10 +15,19 @@ namespace Exercise1
             Console.WriteLine("Neil Dey");
 
             //Part 2 
+            Console.WriteLine("\n \n ");
             Console.WriteLine(13 + 11);
 
             //Part 3
+            Console.WriteLine("\n \n ");
             Console.WriteLine(10 / 2);
+
+            //Part 4
+            Console.WriteLine("\n \n ");
+            Console.WriteLine(-1 + 4 * 6);
+            Console.WriteLine((35 + 5) % 7);
+            Console.WriteLine(14 + -4 * 6 / 11);
+            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
 
             Console.ReadKey();
         }
