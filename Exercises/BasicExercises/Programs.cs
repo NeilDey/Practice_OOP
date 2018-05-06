da66ed3265e67ee6879fad1cb3360d004f601019
Neil Dey <neilrdey@gmail.com>
2018-05-06 10:18:25 +0530

    Basic Exercises : Program 10

--- Exercises/BasicExercises/Programs.cs
+++ Exercises/BasicExercises/Programs.cs
@@ -107,7 +107,22 @@ namespace Exercise1
          number1, number2, number3, number4, result);
 
 
-           
+            //Part 10
+            Console.WriteLine("\n \n ");
+
+            int num1, num2, num3;
+
+            Console.Write("Enter first number - ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number - ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter third number - ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Result of specified numbers {0}, {1} and {2}, (x+y)·z is {3} and x·y + y·z is {4}\n\n",
+                num1, num2, num3, ((num1 + num2) * num3), (num1 * num2 + num2 * num3));
         }
     }
     }
